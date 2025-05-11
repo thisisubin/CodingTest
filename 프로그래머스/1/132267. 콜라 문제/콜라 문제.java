b70741d92e07c74328c05663a1dce340ceebd8dd
thisisubin <127410970+thisisubin@users.noreply.github.com>
2025-05-11 13:00:36 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+       while (n >= a) {
+           int exchange = (n/a) * b;
+           answer += exchange;
+           n = n % a + exchange;
+       }
+        
+        return answer;
+    }
+}
